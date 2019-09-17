73ca7e7e134dac7def22583d9ec765709f7fc21a
Emmy <nuelojay22@gmail.com>
2019-09-17 15:08:17 +0100

    ch: template setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
+import Home from './sreens/home';
 import { hot } from "react-hot-loader";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
     render () {
         return (
-            <div className="App">
-                <h1>Hello Basketball Live!</h1>
-            </div>
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                    </Switch>
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/sreens/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+class Home extends Component {
+    render () {
+        return (
+            <Paper>
+                <Typography>
+          Hello there!
+                </Typography>
+            </Paper>
+        );
+    }
+}
+
+export default Home;
