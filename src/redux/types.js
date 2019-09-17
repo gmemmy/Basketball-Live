c11914117d64c1f58421dc687e8b431f0d283d1e
Emmy <nuelojay22@gmail.com>
2019-09-17 18:10:14 +0100

    ch: setup redux

---
+++ src/redux/types.js
@@ -0,0 +1,6 @@
+// UI Reducer types
+export const SET_ERRORS = "SET_ERRORS";
+export const LOADING_UI = "LOADING-UI";
+export const LOADING_DATA = "LOADING_DATA";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+export const STOP_LOADING_UI = "STOP_LOADING_UI";
